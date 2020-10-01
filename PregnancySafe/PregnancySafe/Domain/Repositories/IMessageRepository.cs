362455a831e71932a9b6415ce79b4fd1efea82b0
JeanPercy <72156492+JeanPercy@users.noreply.github.com>
2020-09-30 22:58:14 -0500

    Creación de Message model
    Creación de Message model

---
+++ PregnancySafe/PregnancySafe/Domain/Repositories/IMessageRepository.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Repositories
+{
+    public interface IMessageRepository
+    {
+        Task<IEnumerable<Message>> ListAsync();
+        Task<Message> FindByIdAsync(int id);
+        Task AddAsync(Message message);
+        void Update(Message message);
+        void Remove(Message message);
+    }
+}
---
+++ PregnancySafe/PregnancySafe/Domain/Services/IMessageService.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+using PregnancySafe.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Services
+{
+    public interface IMessageService
+    {
+        Task<IEnumerable<Message>> ListAsync();
+        //Task<MessageResponse> SaveAsync(Message message);
+        //Task<MessageResponse> UpdateAsync(int id, Message message);
+        //Task<MessageResponse> DeleteAsync(int id);
+    }
+}
