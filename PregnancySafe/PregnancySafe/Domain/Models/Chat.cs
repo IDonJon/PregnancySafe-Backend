df92371b562bad0f930e64f3a52d893778d6b554
donjon01 <71906602+donjon01@users.noreply.github.com>
2020-09-30 22:37:53 -0500

    Creación de Chat Model
    Creación del Chat Model

---
+++ PregnancySafe/PregnancySafe/Domain/Models/Chat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        //public IList<Message> Messages { get; set; } = new List<Message>();
+        public int MotherId { get; set; }
+        public Mother Mother { get; set; }
+        public int ObstetricianId { get; set; }
+        public Obstetrician Obstetrician { get; set; }
+    }
+}
---
+++ PregnancySafe/PregnancySafe/Domain/Services/IChatService.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+using PregnancySafe.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Services
+{
+    public interface IChatService
+    {
+        Task<IEnumerable<Chat>> ListAsync();
+        //Task<ChatResponse> SaveAsync(Chat chat);
+        //Task<ChatResponse> UpdateAsync(int id, Chat chat);
+        //Task<ChatResponse> DeleteAsync(int id);
+    }
+}
