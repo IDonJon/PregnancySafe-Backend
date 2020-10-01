99d3260a9bf9529e79600679eaf4b54ee13ca693
donjon01 <71906602+donjon01@users.noreply.github.com>
2020-09-30 19:28:47 -0500

    Obstetrician Model
    Creación de Obstetrician Model

---
+++ PregnancySafe/PregnancySafe/Domain/Models/Obstetrician.cs
@@ -0,0 +1,23 @@
+﻿using PregnancySafe.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Models
+{
+    public class Obstetrician
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Degree { get; set; }
+        //public IList<Advice> Advices { get; set; } = new List<Advice>();
+        //public IList<Chat> Chats { get; set; } = new List<Chat>();
+        //public IList<MedicalAppointment> MedicalAppointments { get; set; } = new List<MedicalAppointment>();
+        //public IList<MedicalExam> MedicalExams { get; set; } = new List<MedicalExam>();
+        //public IList<Video> Videos { get; set; } = new List<Video>();
+    }
+}
