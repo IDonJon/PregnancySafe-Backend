127c8eea9f670fc3d001af4f4d06587e5ed4a197
donjon01 <71906602+donjon01@users.noreply.github.com>
2020-09-30 23:11:27 -0500

    Creación Model Pregnancy Stage
    Creación del Model Pregnancy Stage

---
+++ PregnancySafe/PregnancySafe/Domain/Models/PregnancyStage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Models
+{
+    public class PregnancyStage
+    {
+        public int Id { get; set; }
+        public string Range { get; set; }
+        public string Description { get; set; }
+        public IList<Mother> Mothers { get; set; } = new List<Mother>();
+    }
+}
---
+++ PregnancySafe/PregnancySafe/Domain/Services/IPregnancyStageService.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+using PregnancySafe.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Services
+{
+    public interface IPregnancyStageService
+    {
+        Task<IEnumerable<PregnancyStage>> ListAsync();
+        //Task<PregnancyStageResponse> SaveAsync(PregnancyStage pregnancyStage);
+        //Task<PregnancyStageResponse> UpdateAsync(int id, PregnancyStage pregnancyStage);
+        //Task<PregnancyStageResponse> DeleteAsync(int id);
+    }
+}
