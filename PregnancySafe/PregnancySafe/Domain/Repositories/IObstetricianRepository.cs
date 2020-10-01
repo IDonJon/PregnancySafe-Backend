cb4b8b5df006da1496b627be3c25c4ab6648cb37
donjon01 <71906602+donjon01@users.noreply.github.com>
2020-09-30 19:35:16 -0500

    Obstetrician Repository y Service
    Creación de Obstetrician Repository y Service

---
+++ PregnancySafe/PregnancySafe/Domain/Repositories/IObstetricianRepository.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Repositories
+{
+    public interface IObstetricianRepository
+    {
+        Task<IEnumerable<Obstetrician>> ListAsync();
+        Task<Obstetrician> FindByIdAsync(int id);
+        Task AddASync(Obstetrician obstetrician);
+        void Update(Obstetrician obstetrician);
+        void Remove(Obstetrician obstetrician);
+    }
+}
---
+++ PregnancySafe/PregnancySafe/Domain/Services/IObstetricianService.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+//using PregnancySafe.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Services
+{
+    public interface IObstetricianService
+    {
+        Task<IEnumerable<Obstetrician>> ListAsync();
+        //Task<ObstetricianResponse> SaveAsync(Obstetrician obstetrician);
+        //Task<ObstetricianResponse> UpdateAsync(int id, Obstetrician obstetrician);
+        //Task<ObstetricianResponse> DeleteAsync(int id);
+    }
+}
