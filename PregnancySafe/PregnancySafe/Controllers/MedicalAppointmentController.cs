4f23b3f34d19b8a0550effd8ab3e40df1906a378
PSTQ <PSTQ@examen.com.pe>
2020-09-30 23:56:40 -0500

    Ampliación medicalappointment
    Resources, controller, services

---
+++ PregnancySafe/PregnancySafe/Controllers/MedicalAppointmentController.cs
@@ -0,0 +1,72 @@
+﻿//using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PregnancySafe.Domain.Models;
+using PregnancySafe.Domain.Services;
+using PregnancySafe.Extensions;
+using PregnancySafe.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Controllers
+{
+    [Route("/pregnancysafe/[controller]")]
+    public class MedicalAppointmentController : Controller
+    {
+        private readonly IMedicalAppointmentService _medicalAppointmentService;
+        //private readonly IMapper _mapper;
+        //public MedicalAppointmentController(IMedicalAppointmentService medicalAppointmentService, IMapper mapper)
+        //{
+        //    _medicalAppointmentService = medicalAppointmentService;
+        //    _mapper = mapper;
+        //}
+
+        //[HttpGet]
+        //public async Task<IEnumerable<MedicalAppointmentResource>> GetAllAsync()
+        //{
+        //    var medicalAppointment = await _medicalAppointmentService.ListAsync();
+        //    var resources = _mapper
+        //        .Map<IEnumerable<MedicalAppointment>, IEnumerable<MedicalAppointmentResource>>(medicalAppointment);
+        //    return resources;
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] SaveMedicalAppointmentResource resource)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.GetErrorMessages());
+            var medicalAppointment = _mapper.Map<SaveMedicalAppointmentResource, MedicalAppointment>(resource);
+            var result = await _medicalAppointmentService.SaveAsync(medicalAppointment);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            var medicalAppointmentResource = _mapper.Map<MedicalAppointment, MedicalAppointmentResource>(result.MedicalAppointment);
+            return Ok(medicalAppointmentResource);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] SaveMedicalAppointmentResource resource)
+        {
+            var medicalAppointment = _mapper.Map<SaveMedicalAppointmentResource, MedicalAppointment>(resource);
+            var result = await _medicalAppointmentService.UpdateAsync(id, medicalAppointment);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var medicalAppointmentResource = _mapper.Map<MedicalAppointment, MedicalAppointmentResource>(result.MedicalAppointment);
+            return Ok(medicalAppointmentResource);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var result = await _medicalAppointmentService.DeleteAsync(id);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+            var medicalAppointmentResource = _mapper.Map<MedicalAppointment, MedicalAppointmentResource>(result.MedicalAppointment);
+            return Ok(medicalAppointmentResource);
+        }
+    }
+}
---
+++ PregnancySafe/PregnancySafe/Services/MedicalAppointmentService.cs
@@ -0,0 +1,77 @@
+﻿using PregnancySafe.Domain.Models;
+using PregnancySafe.Domain.Repositories;
+using PregnancySafe.Domain.Services;
+using PregnancySafe.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Services
+{
+    public class MedicalAppointmentService : IMedicalAppointmentService
+    {
+        private readonly IMedicalAppointmentRepository _medicalAppointmentRepository;
+        public MedicalAppointmentService(IMedicalAppointmentRepository medicalAppointmentRepository)
+        {
+            _medicalAppointmentRepository = medicalAppointmentRepository;
+        }
+
+        public async Task<MedicalAppointmentResponse> DeleteAsync(int id)
+        {
+            var existingMedicalAppointment = await _medicalAppointmentRepository.FindByIdAsync(id);
+
+            if (existingMedicalAppointment == null)
+                return new MedicalAppointmentResponse("Medical Appointment not found");
+
+            try
+            {
+                _medicalAppointmentRepository.Remove(existingMedicalAppointment);
+
+                return new MedicalAppointmentResponse(existingMedicalAppointment);
+            }
+            catch (Exception exception)
+            {
+                return new MedicalAppointmentResponse($"An error ocurred while deleting Medical Appointment: {exception.Message}");
+            }
+        }
+
+        public async Task<IEnumerable<MedicalAppointment>> ListAsync()
+        {
+            return await _medicalAppointmentRepository.ListAsync();
+        }
+
+        public async Task<MedicalAppointmentResponse> SaveAsync(MedicalAppointment medicalAppointment)
+        {
+            try
+            {
+                await _medicalAppointmentRepository.AddASync(medicalAppointment);
+
+                return new MedicalAppointmentResponse(medicalAppointment);
+            }
+            catch (Exception exception)
+            {
+                return new MedicalAppointmentResponse($"An error ocurred while saving Medical Appointment: {exception.Message}");
+            }
+        }
+
+        public async Task<MedicalAppointmentResponse> UpdateAsync(int id, MedicalAppointment medicalAppointment)
+        {
+            var existingMedicalAppointment = await _medicalAppointmentRepository.FindByIdAsync(id);
+
+            if (existingMedicalAppointment == null)
+                return new MedicalAppointmentResponse("Medical Appointment not found");
+
+            try
+            {
+                _medicalAppointmentRepository.Update(existingMedicalAppointment);
+
+                return new MedicalAppointmentResponse(existingMedicalAppointment);
+            }
+            catch (Exception exception)
+            {
+                return new MedicalAppointmentResponse($"An error ocurred while updating Medical Appointment: {exception.Message}");
+            }
+        }
+    }
+}
