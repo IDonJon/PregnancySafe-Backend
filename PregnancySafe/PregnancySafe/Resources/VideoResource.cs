2c08481fb44faa52b35849150520d9dc1b236e45
PSTQ <PSTQ@examen.com.pe>
2020-10-01 00:05:56 -0500

    Ampliacion video
    SaveVideoResource, videoresource, controller, services

---
+++ PregnancySafe/PregnancySafe/Resources/VideoResource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Resources
+{
+    public class VideoResource
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public DateTime PostDate { get; set; }
+        public string Title { get; set; }
+        public ObstetricianResource Obstetrician { get; set; }
+
+    }
+}
