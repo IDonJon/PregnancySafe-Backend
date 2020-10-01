c147059a28219526acae75e095445dc2138a1905
donjon01 <71906602+donjon01@users.noreply.github.com>
2020-09-30 23:58:19 -0500

    Advice

--- PregnancySafe/PregnancySafe/Resources/AdviceResource.cs
+++ PregnancySafe/PregnancySafe/Resources/AdviceResource.cs
@@ -1,74 +1,18 @@
-﻿using PregnancySafe.Domain.Models;
-using PregnancySafe.Domain.Repositories;
-using PregnancySafe.Domain.Services;
-using PregnancySafe.Domain.Services.Communication;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PregnancySafe.Services
+namespace PregnancySafe.Resources
 {
-    public class AdviceService : IAdviceService
+    public class AdviceResource
     {
-        private readonly IAdviceRepository _adviceRepository;
-        public AdviceService(IAdviceRepository adviceRepository)
-        {
-            _adviceRepository = adviceRepository;
-        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime PostDate { get; set; }
+        public ObstetricianResource Obstetrician { get; set; }
 
-        public async Task<AdviceResponse> DeleteAsync(int id)
-        {
-            var existingAdvice = await _adviceRepository.FindByIdAsync(id);
-
-            if (existingAdvice == null)
-                return new AdviceResponse("Advice not found");
-
-            try
-            {
-                _adviceRepository.Remove(existingAdvice);
-                return new AdviceResponse(existingAdvice);
-            }
-            catch (Exception exception)
-            {
-                return new AdviceResponse($"An error ocurred while deleting the Advice: {exception.Message}");
-            }
-        }
-
-        public async Task<IEnumerable<Advice>> ListAsync()
-        {
-            return await _adviceRepository.ListAsync();
-        }
-
-        public async Task<AdviceResponse> SaveAsync(Advice advice)
-        {
-            try
-            {
-                await _adviceRepository.AddASync(advice);
-                return new AdviceResponse(advice);
-            }
-            catch (Exception exception)
-            {
-                return new AdviceResponse($"An error ocurred while saving the Advice: {exception.Message}");
-            }
-        }
-
-        public async Task<AdviceResponse> UpdateAsync(int id, Advice advice)
-        {
-            var existingAdvice = await _adviceRepository.FindByIdAsync(id);
-
-            if (existingAdvice == null)
-                return new AdviceResponse("Advice not found");
-
-            try
-            {
-                _adviceRepository.Update(existingAdvice);
-                return new AdviceResponse(existingAdvice);
-            }
-            catch (Exception exception)
-            {
-                return new AdviceResponse($"An error ocurred while updating the Advice: {exception.Message}");
-            }
-        }
     }
 }
+
