a1066ee2a485e0b0afc07e57b33973e0620ad86f
donjon01 <71906602+donjon01@users.noreply.github.com>
2020-09-30 20:59:34 -0500

    Creación Advice
    Creación del Advice Model

---
+++ PregnancySafe/PregnancySafe/Domain/Repositories/IAdviceRepository.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Repositories
+{
+    public interface IAdviceRepository
+    {
+        Task<IEnumerable<Advice>> ListAsync();
+        Task<Advice> FindByIdAsync(int id);
+        Task AddASync(Advice advice);
+        void Update(Advice advice);
+        void Remove(Advice advice);
+    }
+}
---
+++ PregnancySafe/PregnancySafe/Domain/Services/IAdviceService.cs
@@ -0,0 +1,17 @@
+﻿using PregnancySafe.Domain.Models;
+//using PregnancySafe.Domain.Services.Communication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PregnancySafe.Domain.Services
+{
+    public interface IAdviceService
+    {
+        Task<IEnumerable<Advice>> ListAsync();
+        //Task<AdviceResponse> SaveAsync(Advice advice);
+        //Task<AdviceResponse> UpdateAsync(int id, Advice advice);
+        //Task<AdviceResponse> DeleteAsync(int id);
+    }
+}
